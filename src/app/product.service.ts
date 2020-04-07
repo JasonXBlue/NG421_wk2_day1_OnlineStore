c86c1dd3b98c4f63dc01bc91413903f5c0889069
JasonXBlue <JasonX.blue1@gmail.com>
2020-04-07 17:26:33 -0500

    create product interface and use

---
+++ src/app/product.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@angular/core";
+//import products from "./products";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ProductService {
+  // shoppingCart: Array<Product> = [];
+  // products: Array<Product> = [];
+  // this.products = products;
+  // constructor() { }
+}
