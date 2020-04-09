48e42113a76ce18c734b8a1f9fb05a92d192bcf9
JasonXBlue <JasonX.blue1@gmail.com>
2020-04-08 20:40:09 -0500

    fixed images

--- src/app/products.ts
+++ src/app/products.ts
@@ -7,7 +7,7 @@ const products: IProduct[] = [
     description: "Cleans your hands",
     reviews: 46,
     rating: 2,
-    imgUrl: "http://dummyimage.com/136x167.bmp/cc0000/ffffff",
+    imgUrl: "http://dummyimage.com/320x150.bmp/cc0000/ffffff",
     price: "$95.11",
   },
   {
@@ -16,7 +16,7 @@ const products: IProduct[] = [
     description: "You will be so happy when you use this product",
     reviews: 2,
     rating: 2,
-    imgUrl: "http://dummyimage.com/125x134.jpg/cc0000/ffffff",
+    imgUrl: "http://dummyimage.com/320x150.jpg/cc0000/ffffff",
     price: "$37.09",
   },
   {
@@ -25,7 +25,7 @@ const products: IProduct[] = [
     description: "Tastes hummy",
     reviews: 27,
     rating: 5,
-    imgUrl: "http://dummyimage.com/149x190.jpg/dddddd/000000",
+    imgUrl: "http://dummyimage.com/320x150.jpg/dddddd/000000",
     price: "$51.83",
   },
   {
@@ -34,7 +34,7 @@ const products: IProduct[] = [
     description: "Orchard as the place of occurrence of the external cause",
     reviews: 60,
     rating: 3,
-    imgUrl: "http://dummyimage.com/162x153.jpg/cc0000/ffffff",
+    imgUrl: "http://dummyimage.com/320x150.jpg/cc0000/ffffff",
     price: "$86.93",
   },
   {
@@ -43,7 +43,7 @@ const products: IProduct[] = [
     description: "Kills germs",
     reviews: 20,
     rating: 1,
-    imgUrl: "http://dummyimage.com/120x245.jpg/cc0000/ffffff",
+    imgUrl: "http://dummyimage.com/320x150.jpg/cc0000/ffffff",
     price: "$70.10",
   },
   {
@@ -52,7 +52,7 @@ const products: IProduct[] = [
     description: "this stuff is awesome",
     reviews: 79,
     rating: 3,
-    imgUrl: "http://dummyimage.com/211x227.bmp/5fa2dd/ffffff",
+    imgUrl: "http://dummyimage.com/320x150.bmp/5fa2dd/ffffff",
     price: "$39.25",
   },
   {
@@ -61,7 +61,7 @@ const products: IProduct[] = [
     description: "The best product ever",
     reviews: 76,
     rating: 3,
-    imgUrl: "http://dummyimage.com/212x144.jpg/ff4444/ffffff",
+    imgUrl: "http://dummyimage.com/320x150.jpg/ff4444/ffffff",
     price: "$99.91",
   },
   {
@@ -70,7 +70,7 @@ const products: IProduct[] = [
     description: "Product 3",
     reviews: 74,
     rating: 3,
-    imgUrl: "http://dummyimage.com/204x175.jpg/5fa2dd/ffffff",
+    imgUrl: "http://dummyimage.com/320x150.jpg/5fa2dd/ffffff",
     price: "$67.17",
   },
   {
@@ -79,7 +79,7 @@ const products: IProduct[] = [
     description: " cephalospor/oth beta-lactm antibiot, undet, sequela",
     reviews: 9,
     rating: 1,
-    imgUrl: "http://dummyimage.com/212x108.bmp/cc0000/ffffff",
+    imgUrl: "http://dummyimage.com/320x150.bmp/cc0000/ffffff",
     price: "$96.84",
   },
   {
@@ -88,7 +88,7 @@ const products: IProduct[] = [
     description: "Other Gram-negative sepsis",
     reviews: 45,
     rating: 2,
-    imgUrl: "http://dummyimage.com/189x109.png/cc0000/ffffff",
+    imgUrl: "http://dummyimage.com/320x150.png/cc0000/ffffff",
     price: "$74.37",
   },
 ];
